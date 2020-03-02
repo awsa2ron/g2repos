9c597e6f7b05efed3eec7274a360567d3bf1bb07
Aaron Tsui <kaixu@amazon.com>
2020-03-02 10:38:24 +0800

    add more repos

--- g2repos.sh
+++ g2repos.sh
@@ -4,4 +4,6 @@ echo "grep git repository"
 
 # Clone git repos
 git clone --recurse-submodules --origin upstream git@github.com:awsa2ron/amazon-kinesis-video-streams-producer-c.git
+git clone --recurse-submodules --origin upstream https://github.com/awslabs/amazon-kinesis-video-streams-webrtc-sdk-c.git
+git clone --recurse-submodules --origin upstream https://github.com/curl/curl.git
 
