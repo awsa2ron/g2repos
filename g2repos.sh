1994a19af346bfc6d71877abaac93fcddda839cd
Aaron Tsui <kaixu@amazon.com>
2020-03-02 11:14:22 +0800

    replace git clone options with bash var

--- g2repos.sh
+++ g2repos.sh
@@ -2,8 +2,10 @@
 # Welcome
 echo "grep git repository"
 
+GIT_FLAGS="--recurse-submodules --origin upstream"
+echo $GIT_FLAGS
 # Clone git repos
-git clone --recurse-submodules --origin upstream git@github.com:awsa2ron/amazon-kinesis-video-streams-producer-c.git
-git clone --recurse-submodules --origin upstream https://github.com/awslabs/amazon-kinesis-video-streams-webrtc-sdk-c.git
-git clone --recurse-submodules --origin upstream https://github.com/curl/curl.git
+git clone $GIT_FLAGS git@github.com:awsa2ron/amazon-kinesis-video-streams-producer-c.git
+git clone $GIT_FLAGS https://github.com/awslabs/amazon-kinesis-video-streams-webrtc-sdk-c.git
+git clone $GIT_FLAGS https://github.com/curl/curl.git
 
