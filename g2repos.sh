8a3bae19c899d60bb68e6702be20048cf80e6738
Aaron Tsui <kaixu@amazon.com>
2020-03-02 17:05:26 +0800

    refectoring

--- g2repos.sh
+++ g2repos.sh
@@ -1,21 +1,23 @@
 #! /bin/bash
 # Welcome
 echo "grep git repository"
-if test $# -eq 0
-    then
-        echo "no argument, default config file name is .config"
-        REPOS_FILE=.config
-elif test -f "$1"
-    then
-        REPOS_FILE=$1
-    else
-        echo "not vailid config file"
-        exit 1
+REPOS_FILE=.config
+if [ $# -gt 1 ]; then
+    echo "too many arguments"
+    exit 1
+elif [ $# -eq 0 ]; then
+    echo "use default .config"
+    REPOS_FILE=.config
+elif [ -f "$1"]; then
+    REPOS_FILE=$1
+else
+    echo "not vailid config file"
+    exit 1
 fi
 
         
 GIT_FLAGS=" --recurse-submodules "
-GIT_FLAGS+=" --origin upstream "
+#GIT_FLAGS+=" --origin upstream "
  
 GIT_CLONE="git clone ${GIT_FLAGS}"
 echo ${GIT_CLONE} ...
@@ -25,4 +27,3 @@ for repo in `cat $REPOS_FILE`
 do
     ${GIT_CLONE} $repo
 done
-
