7adc04ca320dc1a5b04c7902099db35ca2444c8d
Aaron Tsui <kaixu@amazon.com>
2020-03-02 10:34:20 +0800

    rename remote origin to upstream

--- g2repos.sh
+++ g2repos.sh
@@ -3,5 +3,5 @@
 echo "grep git repository"
 
 # Clone git repos
-git clone --recurse-submodules git@github.com:awsa2ron/amazon-kinesis-video-streams-producer-c.git
+git clone --recurse-submodules --origin upstream git@github.com:awsa2ron/amazon-kinesis-video-streams-producer-c.git
 
